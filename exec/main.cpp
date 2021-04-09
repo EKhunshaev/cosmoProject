c240c2d697647b40b3d5c6028eaa9d951a3648b7
Elveg Khunshaev <hunshaev.er@phystech.edu>
2021-04-09 17:48:09 +0300

    Инициализация проекта

---
+++ exec/main.cpp
@@ -0,0 +1,6 @@
+#include "../headers/header.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ headers/header.h
@@ -0,0 +1,12 @@
+/*!
+ * @file header.h
+ * @brief Файл содержащий все заголовочные файлы
+ */
+
+#ifndef HEADER_H
+#define HEADER_H
+//Системные
+#include <iostream>
+
+//Локальные
+#endif //HEADER_H
