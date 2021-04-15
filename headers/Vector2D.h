a353a56ae3de05749db29fcd13b7b7ee432942fa
Elveg Khunshaev <hunshaev.er@phystech.edu>
2021-04-15 10:06:25 +0300

    Vector2D/Codestyle + правки в аргументах методов и функций

--- headers/Vector2D.h
+++ headers/Vector2D.h
@@ -3,46 +3,46 @@
 
 class Vector2D
 {
-	double x, y;
+    double x, y;
 public:
-	Vector2D();
-	Vector2D(double x);
-	Vector2D(double x, double y);
+    Vector2D();
+    Vector2D(const double &x);
+    Vector2D(const double &x, const double &y);
 
-	double get_x();
-	double get_y();
+    double get_x();
+    double get_y();
 
-	void set_x(double x);
-	void set_y(double y);
+    void set_x(const double &x);
+    void set_y(const double &y);
 
-	Vector2D *plus(Vector2D *A);
-	Vector2D *minus(Vector2D *A);
+    Vector2D *plus(Vector2D *A);
+    Vector2D *minus(Vector2D *A);
 
-	double mod();
-	
-	friend double mod(Vector2D &A);
-	friend Vector2D operator+ (Vector2D &A, Vector2D &B);
-	friend Vector2D operator- (Vector2D &A, Vector2D &B);
-	friend double operator* (Vector2D &A, Vector2D &B);
-	friend Vector2D operator* (Vector2D &A, double n);
-	friend Vector2D operator* (double n, Vector2D &A);
+    double mod();
 
-	friend Vector2D operator++ (Vector2D &A, int x);
-	friend Vector2D operator-- (Vector2D &A, int x);
-	friend Vector2D operator++ (Vector2D &A);
-        friend Vector2D operator-- (Vector2D &A);
-	friend Vector2D operator+ (Vector2D &A);
-	friend Vector2D operator- (Vector2D &A);
+    friend double mod(const Vector2D &A);
+    friend Vector2D operator+(const Vector2D &A, const Vector2D &B);
+    friend Vector2D operator-(const Vector2D &A, const Vector2D &B);
+    friend double operator*(const Vector2D &A, const Vector2D &B);
+    friend Vector2D operator*(const Vector2D &A, const double &n);
+    friend Vector2D operator*(const double &n, const Vector2D &A);
 
-	friend bool operator== (Vector2D &A, Vector2D &B);
-	friend bool operator!= (Vector2D &A, Vector2D &B);
-	friend bool operator> (Vector2D &A, Vector2D &B);
-	friend bool operator< (Vector2D &A, Vector2D &B);
-	friend bool operator>= (Vector2D &A, Vector2D &B);
-	friend bool operator<= (Vector2D &A, Vector2D &B);
+    friend Vector2D operator++(Vector2D &A, int x);
+    friend Vector2D operator--(Vector2D &A, int x);
+    friend Vector2D operator++(Vector2D &A);
+    friend Vector2D operator--(Vector2D &A);
+    friend Vector2D operator+(Vector2D &A);
+    friend Vector2D operator-(Vector2D &A);
 
-	friend std::ostream &operator << (std::ostream &out, Vector2D &B);
-	friend Vector2D &operator >> (std::istream &out, Vector2D &B);
+    friend bool operator==(const Vector2D &A, const Vector2D &B);
+    friend bool operator!=(const Vector2D &A, const Vector2D &B);
+    friend bool operator>(const Vector2D &A, const Vector2D &B);
+    friend bool operator<(const Vector2D &A, const Vector2D &B);
+    friend bool operator>=(const Vector2D &A, const Vector2D &B);
+    friend bool operator<=(const Vector2D &A, const Vector2D &B);
+
+    friend std::ostream &operator<<(std::ostream &out, const Vector2D &B);
+    friend Vector2D &operator>>(std::istream &in, Vector2D &B);
 
 };
 
